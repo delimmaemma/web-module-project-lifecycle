26df2f12c5c6ec9b270b4d86d7fcf47966d5a45b
delimmaemma <calvinhtml@gmail.com>
2023-07-25 00:47:14 -0500

    Error message added

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,31 +5,39 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   state = {
-    todos: []
+    todos: [],
+    error: '',
   }
 
-  // fetchAllTodos = () => {
-  //   axios.get(URL)
-  //     .then(res => {
-  //       this.setState({
-  //         todos: res.data
-  //       })
-  //     })
-  //     .catch(err => console.log(err))
-  // } Does the same thing as the code below.
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: res.data.data
+        })
+      })
+      .catch(err => {
+          this.setState({...this.state, error: err.response.data.message})
+      })
+  } 
+  // Does the same thing as the code below.
 
   componentDidMount() {
-    fetch(URL)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ ...this.state, todos: data.data })
-      })
+    this.fetchAllTodos()
+    // fetch(URL)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.setState({ ...this.state, todos: data.data })
+    //   })
+    //   .catch(err => {
+    //     this.setState({...this.state, error: err.response.data.message})
+    //   })
   }
 
   render() {
     return (
       <div>
-        <div id='error'>Error: No error here</div>
+        {this.state.error && <div id='error'>Error: {this.state.error}</div>}
         <div id='todos'>
           <h2>Todos:</h2>
           {
